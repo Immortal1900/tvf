cf3ca00689e028a5deee6c4ab1cd3ed23241b351
Immortal9100 <himanshulahare80@gmail.com>
2020-05-03 15:21:59 +0530

    videopicker

--- lib/uploadfiles/uploadimage.dart
+++ lib/uploadfiles/uploadimage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tvf/crud.dart';
 import 'package:tvf/pickfiles/pickimage.dart';
-
+import 'package:tvf/pickfiles/pickvideos.dart';
 import 'package:tvf/setdata/setdata.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -22,6 +22,8 @@ class uploadimage extends StatefulWidget {
 class _uploadimageState extends State<uploadimage> {
   static String downloadurlarr="";
   String downloadurl;
+  static String downloadvurlarr="";
+  String downloadvurl;
   String txtdownloadurl;
   // ignore: must_call_super
   void initState(){
@@ -55,6 +57,24 @@ class _uploadimageState extends State<uploadimage> {
         }
         print("CONCAT URL IS $downloadurlarr");
       }
+      for(int i=0;i<selectedvideo.length;i++){
+        if(selectedvideo!=null){
+          print(1);
+          StorageReference firebasStorageRef=FirebaseStorage.instance.ref().child("blogposts").child("UID")
+              .child("${randomAlphaNumeric(9)}.mp4");
+          print(2);
+          final StorageUploadTask uploadTask=firebasStorageRef.putFile(selectedvideo[i]);
+          print(3);
+          print("UPLOAD STARTED");
+          downloadvurl=await(await uploadTask.onComplete).ref.getDownloadURL();
+          downloadvurlarr=downloadvurlarr+" "+downloadvurl;
+          print(4);
+        }
+        else{
+          print("Video not selected");
+        }
+        print("CONCAT URL IS $downloadvurlarr");
+      }
       try{
         StorageReference firebasStorageRef=FirebaseStorage.instance.ref().child("blogposts").child("UID")
             .child("${randomAlphaNumeric(9)}.txt");
@@ -70,9 +90,10 @@ class _uploadimageState extends State<uploadimage> {
       Map <String,String> blogmap={
         "title":settitle.post_title,
         "authorname":setauthor.author,
+        "desctexturl":txtdownloadurl,
         "description":setdesc.desc,
         "imageurls":downloadurlarr,
-        "desctexturl":txtdownloadurl,
+       "videourls":downloadurlarr
       };
 
 
